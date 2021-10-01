372c6b56eccca7bb47bd9aa009336367b5ce422c
Omar Abdullah <Malfucious97@gmail.com>
2021-10-01 14:08:52 -0500

    Coding exercise maxOfTwo

---
+++ maxOfTwo.js
@@ -0,0 +1,32 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (x < y){
+  console.log(2)
+} else{
+  console.log(1)
+}
+
+
+// Change the values around
+let x = 2
+let x = 1
+
+if (x < y){
+  console.log(2)
+}else{
+  console.log(1)
+}
+
+
+// If the values were the identical
+let x= 2
+let y= 2
+
+if (x >= y){
+  console.log('Values are identical')
+} else{
+  console.log('Values are not identical')
+}
